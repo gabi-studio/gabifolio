40d125749ffc3b57a1a9dacf96d449d9fa945935
Gabi Catalan <gabistudio.dev@gmail.com>
2025-07-13 18:42:02 -0400

    Add home link to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 export default function Header() {
   return (
     <header className="header">
-      <h1>Gabi Catalan - Developer & Product Designer</h1>
+      <h1><a href="/">Gabi Catalan - Developer & Product Designer</a></h1>
         <nav className="header-nav">
             <ul className="header-nav-list">
                 <li><a href="#projects">Projects</a></li>
